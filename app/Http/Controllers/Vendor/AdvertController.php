ecb43b98f6a8da1e192e8714edd5d4cfbfbb3f5c
dreigningking <reigningkingforever@gmail.com>
2023-06-27 14:22:24 +0100

    solve the advert not creating issue

--- app/Http/Controllers/Vendor/AdvertController.php
+++ app/Http/Controllers/Vendor/AdvertController.php
@@ -87,7 +87,8 @@ class AdvertController extends Controller
             'offer'=> $request->offer, 'text_color'=> $request->text_color, 'button_text'=> $request->button_text,
             'button_color'=> $request->button_color,'approved'=> cache('settings')['auto_approve_shop_advert'] ? true:false]);
         } 
-        return redirect()->back()->with(['result'=> 1,'message'=>'Ad Created']);
+        return redirect()->route('vendor.adverts',$adset)->with(['result'=> 1,'message'=>'Ad Created']);
+        // return redirect()->back()->with(['result'=> 1,'message'=>'Ad Created']);
     }
 
     public function edit(Advert $advert){
