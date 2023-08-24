0466ccec6dfe5ccce87a219dd7f2905428c3a6f0
dreigningking <reigningkingforever@gmail.com>
2023-08-24 13:37:33 +0100

    removed wishlist for admin and vendor

--- resources/views/frontend/product/view.blade.php
+++ resources/views/frontend/product/view.blade.php
@@ -201,11 +201,13 @@
                                 
                             @endif
                             @if(Auth::check() && $product->likes->where('user_id',Auth::id())->count() == 1)
-                            <span class="action-btn liked">
-                                <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg" class="remove-from-wish" data-product="{{$product->id}}product">
-                                    <path d="M9.9996 16.5451C-6.66672 7.3333 4.99993 -2.6667 9.9996 3.65668C14.9999 -2.6667 26.6666 7.3333 9.9996 16.5451Z" stroke="currentColor" stroke-width="1.5"></path>
-                                </svg>
-                            </span>
+                                @if(!auth()->check() || auth()->user()->role->name == 'shopper')
+                                <span class="action-btn liked">
+                                    <svg width="20" height="18" viewBox="0 0 20 18" fill="none" xmlns="http://www.w3.org/2000/svg" class="remove-from-wish" data-product="{{$product->id}}product">
+                                        <path d="M9.9996 16.5451C-6.66672 7.3333 4.99993 -2.6667 9.9996 3.65668C14.9999 -2.6667 26.6666 7.3333 9.9996 16.5451Z" stroke="currentColor" stroke-width="1.5"></path>
+                                    </svg>
+                                </span>
+                                @endif
                             @endif
                         </div>
                     </div>
