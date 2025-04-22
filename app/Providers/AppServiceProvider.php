727d6b60f60d5c6425c54e85be012e8910693031
dreigningking <reigningkingforever@gmail.com>
2025-04-22 15:43:56 +0100

    removed changes in appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);  
-        $settings = Cache::rememberForever('settings', function () {
-            return \App\Models\Setting::select(['name','value'])->get()->pluck('value','name')->toArray();
-        });      
+        // Schema::defaultStringLength(191);  
+        // $settings = Cache::rememberForever('settings', function () {
+        //     return \App\Models\Setting::select(['name','value'])->get()->pluck('value','name')->toArray();
+        // });      
     }
 }
